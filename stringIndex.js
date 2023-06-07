4e09c59d59b04dfda1246891bb12ca29f99b5cae
Nirmalrock777 <134360238+Nirmalrock777@users.noreply.github.com>
2023-06-07 15:11:33 +0530

    Add files via upload

--- stringIndex.js
+++ stringIndex.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var http = require('http');
 var t = fs.readFileSync('./forms.html', 'utf-8');
 var server = http.createServer((req, response) => {
-    // response.write('request received from client \n ');
     var url = req.url;
     if (url.indexOf('/home') >= 0) {
         response.end(t);
