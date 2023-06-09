f600df4458dc9d7b854e3d637220d80ffccfdb91
Nirmalrock777 <134360238+Nirmalrock777@users.noreply.github.com>
2023-06-09 19:40:14 +0530

    Add files via upload

--- stringIndex.js
+++ stringIndex.js
@@ -13,8 +13,51 @@ var server = http.createServer((req, response) => {
     }
     else if (url.indexOf('/contact') >= 0) {
         response.end('u r in contact page');
-    } else if (url.indexOf('/') >= 0) {
+    }
+    else if (url.indexOf('/style') >= 0) {
+        fs.readFile('style.css', 'utf-8', (err, data) => {
+            response.end(data);
+        })
+    }
+
+    else if (url.indexOf('/button') > 0) {
+        fs.readFile("./button.js", "utf-8", (err, data) => {
+            if (err) {
+                response.statusCode = 500;
+                response.end("Error reading home.html");
+            } else {
+                response.write(data);
+                response.end();
+            }
+        });
+    }
+    else if (url.indexOf('/users') >= 0) {
+        console.log(url);
+        var queryStr = url.split('?')[1];
+        var parameters = queryStr.split('&');
+        console.log(parameters);
+        for (var i = 0; i < parameters.length; i++) {
+            var param = parameters[i];
+            console.log(param);
+            var paramDetails = param.split("=");
+            console.log(paramDetails);
+        };
+        fs.readFile("./success.html", "utf-8", (err, data) => {
+            if (err) {
+                response.statusCode = 500;
+                response.end("Error reading home.html");
+            } else {
+                response.write(data);
+                response.end();
+            }
+        });
+    }
+
+    else {
         response.end('invalid request');
     }
 });
 server.listen(8000);
+
+
+
